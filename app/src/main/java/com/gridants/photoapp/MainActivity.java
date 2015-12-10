bdd8f8beb1bfb32663b28fedbfe05b7306adcd58
Prathamesh Juvatkar <prathamesh.juvatkar@gmail.com>
2015-12-10 14:07:12 +0530

    loading an image

--- app/src/main/java/com/gridants/photoapp/MainActivity.java
+++ app/src/main/java/com/gridants/photoapp/MainActivity.java
@@ -1,38 +1,62 @@
 package com.gridants.photoapp;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button openPicture;
+    ImageView picture;
+    static final int RESULT_PIC = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        openPicture = (Button) findViewById(R.id.open_picture);
+        picture = (ImageView) findViewById(R.id.picture);
+        openPicture.setOnClickListener(this);
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onClick(View v) {
+        Intent pickPicture =new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickPicture,RESULT_PIC );
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RESULT_PIC && resultCode == RESULT_OK){
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            Picasso.with(getApplicationContext()).load(picturePath).into(picture);
+            //picture.setImageBitmap(BitmapFactory.decodeFile(picturePath));
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/gridants/photoapp/PhotoFragment.java
@@ -0,0 +1,14 @@
+package com.gridants.photoapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+/**
+ * Created by prats on 10/12/15.
+ */
+public class PhotoFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
